f1e544dfaceee5c7db741793c7c1ee587de3f3f0
Kevin Aldama Perez <mkm9376@gmail.com>
2021-06-05 16:08:32 -0500

    fix output path

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///database.db3')
 
-df= pd.read_csv("Downloads/archivo prueba data engineer trainee/clientes.csv",  sep=";")
+print ("Ingrese path del csv") 
+file_name = input()
+df= pd.read_csv(file_name,sep=";")
 df= df.drop(["altura","peso"], axis=1)
 
 df[["fecha_nacimiento","fecha_vencimiento"]] = df[["fecha_nacimiento","fecha_vencimiento"]].apply(pd.to_datetime, format='%Y-%m-%d')
@@ -16,20 +17,23 @@ bins = [20,21,31,41,51,61,100]
 df['age_group'] = pd.cut(df.edad, bins, labels = labels,right=False)
 
 clientes = pd.DataFrame()
-clientes[["fiscal_id","first_name","last_name","gender","birth_date","age","age_group","due_date","delinquency","due_balance","address"]]=
+clientes[["fiscal_id","first_name","last_name","gender","birth_date","age","age_group","due_date","delinquency","due_balance","address"]]= \
   df[["fiscal_id","first_name","last_name","gender","fecha_nacimiento","edad","age_group","fecha_vencimiento","delinquency","deuda","direccion"]]
-clientes[["fiscal_id", "first_name","last_name","gender","address"]] = 
+clientes[["fiscal_id", "first_name","last_name","gender","address"]] = \
   clientes[["fiscal_id", "first_name","last_name","gender","address"]].apply(lambda x: x.astype(str).str.upper())
 clientes["age_group"] = clientes["age_group"].astype("int64")
-writer = pd.ExcelWriter('emails.xlsx', engine='xlsxwriter')
+writer = pd.ExcelWriter('output/clientes.xlsx',datetime_format='dd mm yyyy')
+print(clientes.head())
 df.to_excel(writer)
 writer.save()
+engine = create_engine('sqlite:///database.db3')
 clientes.to_sql("clientes",con=engine)
 
 emails = pd.DataFrame()
 emails [["fiscal_id","email","status","priority"]] = df[["fiscal_id","correo","estatus_contacto","prioridad"]]
 emails [["fiscal_id","email","status"]] = df[["fiscal_id","correo","estatus_contacto"]].apply(lambda x: x.astype(str).str.upper())
 emails ["priority"] = df["prioridad"].fillna(0).astype("int64")
+writer = pd.ExcelWriter('output/emails.xlsx', engine='xlsxwriter')
 df.to_excel(writer)
 writer.save()
 emails.to_sql("emails",con=engine)
@@ -40,6 +44,7 @@ phones [["fiscal_id","phone","status","priority"]] = df[["fiscal_id","telefono",
 phones [["fiscal_id","status"]] = df[["fiscal_id","estatus_contacto"]].apply(lambda x: x.astype(str).str.upper())
 phones ["phone"] = df["telefono"].fillna(0).astype("int64").astype("string")
 phones ["priority"] = df["prioridad"].fillna(0).astype("int64")
+writer = pd.ExcelWriter('output/phones.xlsx', engine='xlsxwriter')
 df.to_excel(writer)
 writer.save()
 phones.to_sql("phones",con=engine)
